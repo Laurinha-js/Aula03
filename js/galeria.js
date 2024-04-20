d959e4300477e6178c66dfd984baaf38925d92c0
Laura <laura.vieira.claro@gmail.com>
2024-04-20 12:04:52 -0300

    Aula 3-If else e for

---
+++ js/galeria.js
@@ -0,0 +1,8 @@
+var miniatura = document.getElementsByClassName("miniatura")[0];
+var fullsize = document.getElementsByClassName("fullzise") [0];
+
+
+for (var i=1; i <=5; i++){
+    miniatura.innerHTML+="<img src=img/foto"+i+".jpg>";
+
+}
---
+++ js/script.js
@@ -0,0 +1,12 @@
+numero = prompt("Digite um número");
+// Vamos calcular o numero digitado em busca do resto da divisão
+// se o resto for igual a 0(zero), então o número o número digitado é par
+// caso contrário, será impar
+
+if(numero % 2 == 0){
+    alert("O número digitado é par");
+}
+
+else{
+    alert("O número digitado é impar");
+}
