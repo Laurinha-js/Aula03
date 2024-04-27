85f8adcf80b0e8f48e7f1147cccbe30855351670
LAURA VIEIRA CLARO <laura.vclaro@senacsp.edu.br>
2024-04-27 11:04:02 -0300

    Finalização da galeria. Alteração do arquivo galeria.js e criação do style.css

--- js/galeria.js
+++ js/galeria.js
@@ -1,8 +1,29 @@
 var miniatura = document.getElementsByClassName("miniatura")[0];
-var fullsize = document.getElementsByClassName("fullzise") [0];
+var fullsize = document.getElementsByClassName("fullsize")[0];
 
 
 for (var i=1; i <=5; i++){
-    miniatura.innerHTML+="<img src=img/foto"+i+".jpg>";
+    miniatura.innerHTML+="<img src=img/foto"+i+".jpg onclick= abrir("+i+")>";
 
 }
+
+// document.getElementsByTagName("img")[0].onclick = function(){
+// fullsize.innerHTML="<img src=img/foto1.jpg>"; 
+// }
+
+// document.getElementsByTagName("img")[1].onclick = function(){
+//     fullsize.innerHTML="<img src=img/foto2.jpg>"; 
+//     }
+
+
+// A função abrir é chamada todas as vezes que uma foto de uma miniatura é clicada. 
+//Quando a miniatura é clicada, ela dispara um evento chamado onclick e chama a função abrir passando por parametro(argumento)um
+// número, que é a posição da foto.
+// Assim, a função abrir recebe o número passado e armazena a variável p. Esta variável é usada para formar o nome da foto que será carregada 
+// na div fullsize como vemos abaixo*/
+
+
+function abrir(p){
+        fullsize.innerHTML="<img src=img/foto"+p+".jpg>"; 
+        }
+
